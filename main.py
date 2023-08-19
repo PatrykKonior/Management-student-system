964580175cf386293b9a0405c88efb197c78aea5
Patryk <patrykkonior4@gmail.com>
2023-08-19 18:13:25 +0200

    Create an About Dialog

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+        about_action.triggered.connect(self.about)
 
         edit_action = QAction(QIcon("icons/search.png"), "Search", self)
         edit_menu_item.addAction(edit_action)
@@ -92,6 +93,21 @@ class MainWindow(QMainWindow):
     def delete(self):
         dialog = DeleteDialog()
         dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super(AboutDialog, self).__init__()
+        self.setWindowTitle("About")
+        content = """
+        This app was created during the course "The Python Mega Course".
+        Feel free to modify and reuse this app.
+        """
+        self.setText(content)
 
 
 class EditDialog(QDialog):
